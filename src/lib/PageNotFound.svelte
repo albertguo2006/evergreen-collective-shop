23a936e8db4d260b124daec823417b4b6efd657c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-13 22:33:21 -0700

    Add 404 page

---
+++ src/lib/PageNotFound.svelte
@@ -0,0 +1,3 @@
+<h1>404 Page Not Found</h1>
+<p>Oops, this is embarrasing - the page you are looking for doesn't exist</p>
+<p>To return to the main page of the store, please click <a href="/">here</a>.</p>
---
+++ src/routes/[...path].svelte
@@ -0,0 +1,5 @@
+<script>
+import PageNotFound from "$lib/PageNotFound.svelte";
+
+</script>
+<PageNotFound/>
