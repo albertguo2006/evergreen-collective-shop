8929d858c776df8cbb596e77a5ebcdd2f5b5299b
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-14 12:01:08 -0700

    You've heard about a paywall, so how about an authWall?

---
+++ src/lib/authWall.svelte
@@ -0,0 +1,2 @@
+<h2>Oops, you've stumbled onto an admin page. Should you wish to go back to the main shop, click <a href="/">here</a>.</h2>
+<p>Are you an admin? <a href="/admin/login">Login</a></p>
