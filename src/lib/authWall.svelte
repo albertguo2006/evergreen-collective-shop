a36158b3bc95c671d0f567623eecab2636877a34
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-15 10:24:36 -0700

    Style up Auth Wall

--- src/lib/authWall.svelte
+++ src/lib/authWall.svelte
@@ -1,2 +1,7 @@
-<h2>Oops, you've stumbled onto an admin page. Should you wish to go back to the main shop, click <a href="/">here</a>.</h2>
-<p>Are you an admin? <a href="/admin/login">Login</a></p>
+<article class="h-screen grid grid-column-1 place-items-center text-center">
+	<div class="top-1/4 text-3xl font-extrabold text-stone-900 mx-5">
+		<h2>Oops, you've stumbled onto an admin page.</h2>
+		<h2>Should you wish to go back to the main shop, click <a href="/" class="text-blue-600">here</a>.</h2>
+	</div>
+	<a href="/admin/login" class="top-3/4"><p>Are you an admin? Login</p></a>
+</article>
