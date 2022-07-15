549d718a12afdf64a1d53eb6ec86f8211bc900f0
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-15 10:16:55 -0700

    Change shade of blue in links

--- src/lib/PageNotFound.svelte
+++ src/lib/PageNotFound.svelte
@@ -2,6 +2,6 @@
 	<h1 class="top-1/4 text-5xl font-extrabold text-stone-900 ">404 Page Not Found</h1>
 	<div class="top-1/2 text-3xl font-bold text-stone-900">
 		<p>Oops, this is embarrasing - the page you are looking for doesn't exist</p>
-		<p>To return to the main page of the store, please click <a href="/" class="text-blue-700">here</a>.</p>
+		<p>To return to the main page of the store, please click <a href="/" class="text-blue-600">here</a>.</p>
 	</div>
 </article>
