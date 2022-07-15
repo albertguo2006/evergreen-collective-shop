60402367d0cd0bed527ab0307f5d720262f7f065
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-15 00:44:12 -0700

    Add css for 404 screen

--- src/lib/PageNotFound.svelte
+++ src/lib/PageNotFound.svelte
@@ -1,3 +1,8 @@
-<h1>404 Page Not Found</h1>
-<p>Oops, this is embarrasing - the page you are looking for doesn't exist</p>
-<p>To return to the main page of the store, please click <a href="/">here</a>.</p>
+<!-- We use absolute in the css. Ensure that any other elements are not covered by the 404 (there should not be any other elements, baring the __layout) -->
+<article class="h-screen grid grid-column-1 place-items-center text-center">
+	<h1 class="absolute top-1/4 text-5xl font-extrabold text-stone-900 ">404 Page Not Found</h1>
+	<div class="absolute top-1/2 text-3xl font-bold text-stone-900">
+		<p>Oops, this is embarrasing - the page you are looking for doesn't exist</p>
+		<p>To return to the main page of the store, please click <a href="/" class="text-blue-700">here</a>.</p>
+	</div>
+</article>
