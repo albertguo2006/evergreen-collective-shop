36aaa059df42c11ce34b70527078707a8fed42b5
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-16 18:02:35 -0700

    Fix colour of links changing when hovering on nav bar instead of on links

--- src/routes/admin/__layout@root.svelte
+++ src/routes/admin/__layout@root.svelte
@@ -2,11 +2,10 @@
 	export const prerender = true;
 </script>
 
-
-<nav class="grid grid-cols-3 h-20 content-center bg-neutral-700 text-slate-100 hover:text-indigo-200">
-	<a href="/admin/dash" class="col-start-2 place-self-center">
+<nav class="grid grid-cols-3 h-20 content-center bg-neutral-700 text-slate-100">
+	<a href="/admin/dash" class="col-start-2 place-self-center hover:text-indigo-200">
 		<h2 class="text-xl font-extrabold text-center">Admin Panel</h2>
 	</a>
-	<a href="/" class="col-start-3 place-self-end text-right text-lg mr-6">Back to user site</a>
+	<a href="/" class="col-start-3 place-self-end text-right text-lg mr-6 hover:text-indigo-200">Back to user site</a>
 </nav>
 <slot />
