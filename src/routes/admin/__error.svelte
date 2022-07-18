fb389b64c6db9bed7dc7df709fa34c41d89fb44b
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-18 16:59:53 -0700

    Update error page to reflect

--- src/routes/admin/__error.svelte
+++ src/routes/admin/__error.svelte
@@ -1,5 +1,21 @@
-<script lang="ts">
+<script lang="ts" context="module">
 	import AuthWall from '$lib/authWall.svelte';
+	import type { Load } from '@sveltejs/kit';
+
+    let errorCode: number | undefined | null;
+
+	export const load: Load = async ({ status }) => {
+        errorCode = status;
+        return {
+            code: 200
+        }
+    };
 </script>
 
+{#if errorCode === 401 || errorCode === 403}
 <AuthWall />
+{:else if errorCode !== undefined && errorCode !== null}
+<article class="h-screen grid grid-column-1 place-items-center text-center">
+	<h1 class="top-1/4 text-5xl font-extrabold text-stone-900 ">An unexpected error occurred</h1>
+</article>
+{/if}
