2600e3f8c134ccab1e4ba9b24836b7dafe76ab9e
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-17 22:10:34 -0700

    Move inventory api calls to a separate api route

--- src/routes/api/public/items/[item].ts
+++ src/routes/api/public/items/[item].ts
@@ -1,18 +1,10 @@
 import clientPromise from "$lib/mongoDB";
-import dotenv from "dotenv";
 import type { ParamMatcher, RequestHandler } from "@sveltejs/kit";
 
 export const match: ParamMatcher = (param) => {
-    switch (param.toLowerCase()) {
-        case "tote-bag":
-        case "another-item":
-            return true;
-        default:
-            return false;
-    }
-}
-
-dotenv.config();
+    if (param.length == 0) return false;
+    return true;
+} 
 
 export const get: RequestHandler = async ({ params }) => {
 
@@ -20,10 +12,11 @@ export const get: RequestHandler = async ({ params }) => {
 
     const db = dbConnection.db(process.env["DB_NAME"]);
     const collection = db.collection(process.env["DB_STOCK_COLLECTION"] as string);
-    const itemStock = await collection.findOne({ name: params["item"] });
+    const itemStock = await collection.findOne({ name: params["item"].toLowerCase() });
 
     return {
         status: 200,
-        body: JSON.parse(JSON.stringify(itemStock)) // Stringify and Parse to make it JSON, cause ItemStock is not a valid return type
+        body: itemStock 
     };
-};
+
+}
---
+++ src/routes/api/public/items/index.ts
@@ -0,0 +1,18 @@
+import clientPromise from "$lib/mongoDB";
+import dotenv from "dotenv";
+import type { RequestHandler } from "@sveltejs/kit";
+
+dotenv.config();
+
+export const get: RequestHandler = async () => {
+
+    const dbConnection = await clientPromise;
+    const db = dbConnection.db(process.env["DB_NAME"]);
+    const collection = db.collection(process.env["DB_STOCK_COLLECTION"] as string);
+    const itemStock = await collection.find().toArray();
+
+    return {
+        status: 200,
+        body: itemStock
+    }
+}
