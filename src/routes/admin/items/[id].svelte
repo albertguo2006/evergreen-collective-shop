27c5422227ffed3231f71c2c00d71cee382ba5a0
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-19 00:41:03 -0700

    Clean up code + attempt fix for smaller devices

--- src/routes/admin/items/[id].svelte
+++ src/routes/admin/items/[id].svelte
@@ -55,9 +55,37 @@
 		editedSold = originalItem?.sold;
 	});
 
+	function stringCheck(original: any, edited: any): boolean {
+		return typeof edited == 'string' && edited !== '' && original !== edited;
+	}
+
+	function wholeNumberCheck(original: any, edited: any): boolean {
+		return (
+			typeof edited == 'number' &&
+			original !== edited &&
+			edited >= 0 && // Don't allow negative numbers
+			!edited.toString().includes('.') // Don't allow decimals
+		);
+	}
+
+	function isUnlimitedCheck(original: any, edited: any, stock: any): boolean {
+		if (!(typeof edited == 'boolean')) return false;
+
+		if (edited) {
+			return original !== edited;
+		} else {
+			return (
+				original !== edited &&
+				typeof stock == 'number' &&
+				stock > 0 &&
+				!stock.toString().includes('.')
+			);
+		}
+	}
+
 	async function submitChanges() {
 		const confirmed = confirm(
-			'Are you sure that you want to submit these changes? This action is irreversible.'
+			'Are you sure that you want to submit these changes? The consequences may be irreversible.'
 		);
 		if (!confirmed) return;
 		if (objectId === undefined) {
@@ -66,37 +94,17 @@
 		}
 
 		let body: any = {}; //We don't really have any way to type this
-		if (typeof editedName === 'string' && editedName !== '' && editedName !== originalItem?.name) {
-			body.name = editedName;
-		}
-
-		if (typeof editedSold === 'number' && editedSold !== originalItem?.sold) {
-			body.sold = editedSold;
-		}
-
-		if (editedUnlimited && editedUnlimited !== originalItem?.isUnlimited) {
-			body.sold = editedSold;
-		}
-
-		if (
-			!editedUnlimited &&
-			typeof editedStock === 'number' &&
-			editedUnlimited != originalItem?.isUnlimited
-		) {
+		if (stringCheck(originalItem?.name, editedName)) body.name = editedName;
+		if (wholeNumberCheck(originalItem?.currentPriceCents, editedPrice))
+			body.currentPriceCents = editedPrice;
+		if (isUnlimitedCheck(originalItem?.isUnlimited, editedUnlimited, editedStock))
 			body.isUnlimited = editedUnlimited;
-		}
-
-		if (
-			typeof editedStock === 'number' &&
-			!editedUnlimited &&
-			editedStock !== originalItem?.originalStockIfLimited
-		) {
+		if (body.isUnlimited) {
+			body.originalStockIfLimited = null;
+		} else if (wholeNumberCheck(originalItem?.originalStockIfLimited, editedStock)) {
 			body.originalStockIfLimited = editedStock;
 		}
-
-		if (typeof editedPrice === 'number' && editedPrice !== originalItem?.currentPriceCents) {
-			body.currentPriceCents = editedPrice;
-		}
+		if (wholeNumberCheck(originalItem?.sold, editedSold)) body.sold = editedSold;
 
 		const update = await fetch(`/api/protected/items/${objectId}`, {
 			method: 'PATCH',
@@ -117,49 +125,61 @@
 
 {#if isAuthorized == true}
 	{#if originalItem !== undefined && editedPrice !== undefined && editedUnlimited !== undefined && editedStock !== undefined && editedSold !== undefined}
-		<div class="flex flex-col xl:flex-row gap-10 justify-center mx-4 mt-10">
-			<div class="flex flex-col w-11/12 xl:w-1/3 self-center xl:self-auto p-4">
+		<div class="flex flex-col 2xl:flex-row gap-10 justify-center mx-4 mt-10">
+			<div class="flex flex-col w-11/12 2xl:w-1/3 self-center 2xl:self-auto p-4">
 				<img
 					src="/images/{objectId}.png"
 					alt="A picture of a(n) {originalItem.name}"
-					class="border-4 border-slate-500 rounded self-stretch"
+					class="border-4 border-slate-500 rounded h-5/12 self-center"
 				/>
 			</div>
 			<div
-				class="flex flex-col w-11/12 xl:w-1/2 self-center xl:self-auto p-4 text-xl justify-center"
+				class="flex flex-col w-11/12 2xl:w-1/2 self-center 2xl:self-auto text-xs md:text-xl 2xl:text-2xl p-4 justify-center min-w-max"
 			>
 				<div class="grid grid-cols-2 mt-5 border-2 p-4 border-slate-500 rounded-lg">
-					<div class="m-2">
+					<div class="m-2 space-y-2">
+						<!-- `&nbsp` is the code for a whitespace character -->
 						<h2>Interal Object id:</h2>
-						<h2>Item Name</h2>
-						<h2>Number of {originalItem.name}s sold:</h2>
-						<h2>Has unlimited stock:</h2>
-						{#if !editedUnlimited}
-							<h2>Original Stock:</h2>
-						{/if}
-						<h2>Current price in cents:</h2>
-					</div>
-					<!-- <h2>Internal Object Id: {item._id}</h2>
-					<h2>Name: {item.name}</h2> -->
+						<h2>
+							Item Name{#if stringCheck(originalItem.name, editedName)}
+								&nbsp(was {originalItem.name}){/if}:
+						</h2>
+						<h2>
+							Current price in cents{#if wholeNumberCheck(originalItem.currentPriceCents, editedPrice)}
+								&nbsp(was {originalItem.currentPriceCents}){/if}:
+						</h2>
+						<h2>
+							Has unlimited stock{#if isUnlimitedCheck(originalItem.isUnlimited, editedUnlimited, editedStock)}
+								&nbsp(was {originalItem.isUnlimited}){/if}:
+						</h2>
 
-					<div class="m-2">
+						{#if !editedUnlimited}
+							<h2>
+								Original Stock{#if wholeNumberCheck(originalItem.originalStockIfLimited, editedStock)}
+									&nbsp(was {originalItem.originalStockIfLimited}){/if}:
+							</h2>
+						{/if}
+						<h2>
+							Number of {originalItem.name}s sold{#if wholeNumberCheck(originalItem.sold, editedSold)}
+								&nbsp(was {originalItem.sold}){/if}:
+						</h2>
+					</div>
+
+					<div class="m-2 space-y-2">
 						<h2>{originalItem._id}</h2>
 						<h2><input type="text" bind:value={editedName} class="bg-gray-300" /></h2>
-						<h2><input type="number" bind:value={editedSold} class="bg-gray-300" /></h2>
+						<h2><input type="number" bind:value={editedPrice} class="bg-gray-300" /></h2>
 						<h2><input type="checkbox" bind:checked={editedUnlimited} class="h-5" /></h2>
 						{#if !editedUnlimited}
 							<h2>
 								<input type="number" bind:value={editedStock} class="bg-gray-300" />
 							</h2>
 						{/if}
-						<h2>
-							<input type="number" bind:value={editedPrice} class="bg-gray-300" />
-						</h2>
+						<h2><input type="number" bind:value={editedSold} class="bg-gray-300" /></h2>
 					</div>
 				</div>
 
-				<!-- Need a special check to ensure that when edited unlimited is false, the stock number is non-null, on top of all other checks -->
-				{#if (typeof editedName === 'string' && editedName !== '' && editedName !== originalItem.name) || (typeof editedSold === 'number' && editedSold !== originalItem.sold) || (editedUnlimited && editedUnlimited !== originalItem.isUnlimited) || (!editedUnlimited && typeof editedStock === 'number' && editedUnlimited != originalItem.isUnlimited) || (typeof editedStock === 'number' && !editedUnlimited && editedStock !== originalItem.originalStockIfLimited) || (typeof editedPrice === 'number' && editedPrice !== originalItem.currentPriceCents)}
+				{#if stringCheck(originalItem.name, editedName) || wholeNumberCheck(originalItem.currentPriceCents, editedPrice) || isUnlimitedCheck(originalItem.isUnlimited, editedUnlimited, editedStock) || (!isUnlimitedCheck && wholeNumberCheck(originalItem?.originalStockIfLimited, editedStock)) || wholeNumberCheck(originalItem.sold, editedSold)}
 					<button
 						type="submit"
 						class="block h-12 mt-2 rounded-lg bg-red-500"
@@ -177,9 +197,3 @@
 {:else if isAuthorized == false}
 	<AuthWall />
 {/if}
-
-<style>
-	h2 {
-		margin: 0.5rem; /*Equivilant to `margin-2`*/
-	}
-</style>
