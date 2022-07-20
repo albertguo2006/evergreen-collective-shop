e848f4dc2a6b97faaec0bed5a05ef3096cef2db7
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-19 17:47:17 -0700

    Change types

--- src/lib/Purchase.ts
+++ src/lib/Purchase.ts
@@ -1,9 +1,16 @@
 import type { ObjectId } from "mongodb";
 
 export class Purchase {
-    constructor(public name: string, public email: string, public products: Item[], public contacted: boolean, public pickupArranged: boolean, public pickupCompleted: boolean, public _id?: ObjectId) {}
+    constructor(public name: string, public email: string, public products: Product[], public status: PurchaseStatus, public public _id?: ObjectId) {}
 }
 
-export class Item {
-    constructor(public name: string, public priceCentsAtSale: number, public quantity: number, public _id?: ObjectId) {}
+export class Product {
+    constructor(public name: string, public priceCentsAtSale: number, public quantity: number) {}
+}
+
+export enum PurchaseStatus {
+    NeedsContacting = "needsContacting",
+    Contacted = "contacted",
+    PickupArranged = "pickupArranged",
+    Completed = "completed"
 }
