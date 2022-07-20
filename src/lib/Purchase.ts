873553a8625bc8bdc9e93b34e26527b9832c4bd2
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-19 17:48:53 -0700

    Fix typo

--- src/lib/Purchase.ts
+++ src/lib/Purchase.ts
@@ -1,7 +1,7 @@
 import type { ObjectId } from "mongodb";
 
 export class Purchase {
-    constructor(public name: string, public email: string, public products: Product[], public status: PurchaseStatus, public public _id?: ObjectId) {}
+    constructor(public name: string, public email: string, public products: Product[], public status: PurchaseStatus, public _id?: ObjectId) {}
 }
 
 export class Product {
