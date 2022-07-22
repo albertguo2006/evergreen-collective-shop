9e71243c9920050bae1cda06b284a301983c9b7c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-21 20:25:33 -0700

    Add description field

--- src/lib/ItemStock.ts
+++ src/lib/ItemStock.ts
@@ -3,6 +3,7 @@ import type { ObjectId } from "mongodb";
 export default class ItemStock {
     constructor(
         public name: string,
+        public description: string,
         public sold: number,
         public isUnlimited: boolean,
         public originalStockIfLimited: number | null,
