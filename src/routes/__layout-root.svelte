7a3ebdd1743986dc9f36691819f3d9319be39f11
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-21 20:31:10 -0700

    Make background colour appear on all of screen

--- src/routes/__layout-root.svelte
+++ src/routes/__layout-root.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <!-- Reset to almost a blank slate. Admins don't need things like the cart or the socials footer -->
-<body class="h-screen bg-slate-200">
+<body class="min-h-screen bg-slate-200">
 	<slot />
 </body>
