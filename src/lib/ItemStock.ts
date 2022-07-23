fa6e477a25c900bab67b86ace736213f84001c6c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-23 11:51:02 -0700

    Remove description field from ItemStock

--- src/lib/ItemStock.ts
+++ src/lib/ItemStock.ts
@@ -3,7 +3,6 @@ import type { ObjectId } from "mongodb";
 export default class ItemStock {
     constructor(
         public name: string,
-        public description: string,
         public sold: number,
         public isUnlimited: boolean,
         public originalStockIfLimited: number | null,
